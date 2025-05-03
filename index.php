5b5b5c19e9a4a8c9faa7b2c8e8df0502ae21fa1a
Solo Coder <148307188+ShanMihirangaHerath@users.noreply.github.com>
2025-05-03 14:01:02 +0530

    Add assets

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
 <!doctype html>
 <html>
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="./src/output.css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./src/assets/images/walawa-official-logo.png">
+    <title>Home | Walawa Cabana Lake Resort</title>
+    <!-- Style -->
+    <link href="./src/output.css" rel="stylesheet">
+    <link rel="stylesheet" href="./src/css/style.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 </head>
+
 <body>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <h1 class="text-3xl font-bold underline tagesschrift-en">
+        Hello world!
+    </h1>
 </body>
+
 </html>
